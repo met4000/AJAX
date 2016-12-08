777091b037ab7069d33cf03908df0f9b715aa465
N.R.T <nathsharwor@hotmail.com>
2016-12-08 12:25:12 +0800

    Add communication (object) and AJAX.get()

--- ajax.lib.js
+++ ajax.lib.js
@@ -1,1 +1,25 @@
-
+function Communication(){}
+var AJAX = new Communication();
+//data is optional
+AJAX.prototype.get = function (target, set, data) {
+	if (target === undefined) { target = "index.php"; }
+	if (set === undefined) { set = false; }
+	if (data === undefined) { data = ""; }
+	var xhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+	if (!!set) {
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4) {
+				if (this.status == 200)
+					eval(set + "= this.responseText");
+				else
+					console.error("AJAX error:\nError " + this.status + ": " + this.statusText + "\nMode: Async\nReceive Target: " + set + "\nRT Value: " + eval(set) + "\nTarget: " + target + "\nData: " + data + "\n\nPlease send this error to the developers if this page.");
+			}
+		};
+	}
+	xhttp.open("GET", "ajax_info.txt", !!set);
+	xhttp.send();
+	if (!set) { return this.responseText; }
+}
+AJAX.prototype.post = function (target, set, data) {
+	
+}
